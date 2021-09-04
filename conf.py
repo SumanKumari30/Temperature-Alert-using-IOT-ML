5b347bdce552623a7166106ed364f656fa9099d4
SumanKumari30 <70462459+SumanKumari30@users.noreply.github.com>
2021-09-04 21:58:10 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> SID = 'AC5645f61df50abd639026df881e89daa1'
+AUTH_TOKEN = 'e2024f1d108687efe19db34a74443bee'
+FROM_NUMBER = '+17032918270'
+TO_NUMBER = '+919749522710'
+API_KEY = '2f794fd6-f7e3-413c-8dbf-eff43f335184'
+DEVICE_ID = 'BOLT6907221'
